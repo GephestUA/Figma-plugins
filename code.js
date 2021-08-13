42ea257a76de33864956a2d64bd8b74868d880ea
GephestUA <gephestua@gmail.com>
2021-08-13 13:03:24 +0300

    create label x and margin with border

--- code.js
+++ code.js
@@ -13,16 +13,12 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-function isOdd(num) {
-    return num % 2;
-}
 figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
     if (msg.type === 'BarChart') {
         yield figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
-        let { countColumns, minValue, maxValue, columnWidth } = msg;
+        let { countColumns, minValue, maxValue, columnWidth, chartCount } = msg;
         let chartWidth = columnWidth * countColumns + 50 * countColumns;
         const step = String(maxValue).length === 3 ? 20 : 100;
-        let chartCount = 3;
         columnWidth = columnWidth / chartCount;
         const minusMinValue = minValue < 0
             ? Number.isInteger(minValue / step)
@@ -40,29 +36,49 @@ figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
         baseRect.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 0.2 } }];
         const group = figma.group([baseRect], figma.currentPage);
         // Create Bar
-        const qwe = new Array(chartCount);
-        qwe.fill(1, 0, chartCount);
-        let rightDirection = 50 / 2;
-        let leftDirection = 50 / 2;
-        qwe.forEach((_, index) => {
+        const iterateChart = new Array(chartCount);
+        iterateChart.fill(1, 0, chartCount);
+        iterateChart.forEach((_, index) => {
             for (let i = 0; i < countColumns; i++) {
                 let rect = figma.createRectangle();
                 const rectHight = getRandomInt(randomMinValue, randomMaxValue);
+                rect.strokeWeight = 1;
+                rect.strokeCap;
                 rect.resize(columnWidth, Math.abs(rectHight));
-                if (index === 0) {
-                    rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.68, b: 0.75 } }];
-                }
-                else {
-                    rect.fills = [{ type: 'SOLID', color: { r: Math.random(), g: Math.random(), b: Math.random() } }];
-                }
-                rect.x = i * (columnWidth * chartCount + 50) + (50 / 2 + columnWidth * index);
+                const chartStrokeStyles = [
+                    {
+                        color: { r: 0, g: 0, b: 0.2 },
+                        type: 'SOLID',
+                        visible: true,
+                        opacity: 0.7,
+                        blendMode: 'NORMAL',
+                    },
+                ];
+                rect.strokes = rect.strokes.concat(chartStrokeStyles);
+                rect.fills = [{ type: 'SOLID', color: { r: Math.random(), g: Math.random(), b: Math.random() } }];
+                rect.x = i * (columnWidth * chartCount + 50) + (50 / 2 + columnWidth * index) + index / 2;
                 Math.sign(rectHight) === -1 ? (rect.y = 0.5) : (rect.y = -rectHight);
                 group.appendChild(rect);
             }
         });
+        // Create Label X
+        for (let i = 0; i < countColumns; i++) {
+            const sectionWidth = chartWidth / countColumns;
+            const labels = figma.createText();
+            labels.characters = String(labels);
+            labels.name = 'Label-X';
+            labels.characters = 'TEXT';
+            labels.fills = [{ type: 'SOLID', color: { r: 0.49, g: 0.49, b: 0.49 } }];
+            labels.fontSize = 8;
+            labels.resize(sectionWidth, 10);
+            labels.textAlignHorizontal = 'CENTER';
+            labels.y = minusMinValue + 5;
+            labels.x = sectionWidth * i;
+            group.appendChild(labels);
+        }
+        // Create Label Y and horizontal grid
         let label = 0;
         let horizontalGrids = 0;
-        // Create Label Y and horizontal grid
         for (let i = 0; i <= numberOfLabel; i++) {
             // Create Label Y
             const labels = figma.createText();
@@ -79,6 +95,7 @@ figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
             }
             // Create Label Y
             labels.characters = String(label);
+            labels.name = 'Label-Y';
             labels.x = String(maxValue).length * -10;
             labels.y = Number(-label) - 4;
             labels.fills = [{ type: 'SOLID', color: { r: 0.49, g: 0.49, b: 0.49 } }];
--- code.ts
+++ code.ts
@@ -6,19 +6,13 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function isOdd(num) {
-  return num % 2;
-}
-
 figma.ui.onmessage = async (msg) => {
   if (msg.type === 'BarChart') {
     await figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
 
-    let { countColumns, minValue, maxValue, columnWidth } = msg;
+    let { countColumns, minValue, maxValue, columnWidth, chartCount } = msg;
     let chartWidth = columnWidth * countColumns + 50 * countColumns;
     const step = String(maxValue).length === 3 ? 20 : 100;
-
-    let chartCount = 3;
 
     columnWidth = columnWidth / chartCount;
     const minusMinValue =
@@ -43,23 +37,30 @@ figma.ui.onmessage = async (msg) => {
 
     // Create Bar
 
-    const qwe = new Array(chartCount);
-    qwe.fill(1, 0, chartCount);
+    const iterateChart = new Array(chartCount);
+    iterateChart.fill(1, 0, chartCount);
 
-    let rightDirection = 50 / 2;
-    let leftDirection = 50 / 2;
-    qwe.forEach((_, index) => {
+    iterateChart.forEach((_, index) => {
       for (let i = 0; i < countColumns; i++) {
         let rect = figma.createRectangle();
         const rectHight = getRandomInt(randomMinValue, randomMaxValue);
-
+        rect.strokeWeight = 1;
+        rect.strokeCap;
         rect.resize(columnWidth, Math.abs(rectHight));
-        if (index === 0) {
-          rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.68, b: 0.75 } }];
-        } else {
-          rect.fills = [{ type: 'SOLID', color: { r: Math.random(), g: Math.random(), b: Math.random() } }];
-        }
-        rect.x = i * (columnWidth * chartCount + 50) + (50 / 2 + columnWidth * index);
+
+        const chartStrokeStyles: ConcatArray<Paint> = [
+          {
+            color: { r: 0, g: 0, b: 0.2 },
+            type: 'SOLID',
+            visible: true,
+            opacity: 0.7,
+            blendMode: 'NORMAL',
+          },
+        ];
+        rect.strokes = rect.strokes.concat(chartStrokeStyles);
+        rect.fills = [{ type: 'SOLID', color: { r: Math.random(), g: Math.random(), b: Math.random() } }];
+
+        rect.x = i * (columnWidth * chartCount + 50) + (50 / 2 + columnWidth * index) + index / 2;
 
         Math.sign(rectHight) === -1 ? (rect.y = 0.5) : (rect.y = -rectHight);
 
@@ -67,10 +68,29 @@ figma.ui.onmessage = async (msg) => {
       }
     });
 
+    // Create Label X
+
+    for (let i = 0; i < countColumns; i++) {
+      const sectionWidth = chartWidth / countColumns;
+      const labels = figma.createText();
+      labels.characters = String(labels);
+      labels.name = 'Label-X';
+      labels.characters = 'TEXT';
+      labels.fills = [{ type: 'SOLID', color: { r: 0.49, g: 0.49, b: 0.49 } }];
+      labels.fontSize = 8;
+      labels.resize(sectionWidth, 10);
+      labels.textAlignHorizontal = 'CENTER';
+
+      labels.y = minusMinValue + 5;
+      labels.x = sectionWidth * i;
+
+      group.appendChild(labels);
+    }
+
+    // Create Label Y and horizontal grid
     let label = 0;
     let horizontalGrids = 0;
 
-    // Create Label Y and horizontal grid
     for (let i = 0; i <= numberOfLabel; i++) {
       // Create Label Y
       const labels = figma.createText();
@@ -86,6 +106,7 @@ figma.ui.onmessage = async (msg) => {
       }
       // Create Label Y
       labels.characters = String(label);
+      labels.name = 'Label-Y';
       labels.x = String(maxValue).length * -10;
       labels.y = Number(-label) - 4;
       labels.fills = [{ type: 'SOLID', color: { r: 0.49, g: 0.49, b: 0.49 } }];
